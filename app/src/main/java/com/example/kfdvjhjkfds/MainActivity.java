bc6883b01884c8fd8459f36242768850f5865111
anastasiysharova <anastasiysharova1910@gmail.com>
2024-03-30 14:08:38 +0300

    Добавлено кнопка обратно

--- app/src/main/java/com/example/kfdvjhjkfds/MainActivity.java
+++ app/src/main/java/com/example/kfdvjhjkfds/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kfdvjhjkfds;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     Button clickButton;
+    Button minusClick;
     TextView TextClick;
 
     ImageView cot1;
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private long score = 0;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +35,10 @@ public class MainActivity extends AppCompatActivity {
 
         TextClick = (TextView) findViewById(R.id.TextClick);
         clickButton = (Button) findViewById(R.id.clickButton);
+        minusClick = (Button) findViewById(R.id.minusClick);
 
         clickButton.setOnClickListener(clickListener);
+        minusClick.setOnClickListener(clickListener2);
 
     };
 
@@ -44,30 +49,54 @@ public class MainActivity extends AppCompatActivity {
 
     View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
-        public void onClick(View v)
-        {
+        public void onClick(View v) {
             score++;
-             String s = "Кликов: " + score;
+            String s = "Кликов: " + score;
 
-             TextClick.setText(s.toCharArray(),0, s.length());
+            TextClick.setText(s.toCharArray(), 0, s.length());
             ImageView cat1 = findViewById(R.id.cat1);
-             if (score == 10)
-             {
-                 cat1.setImageResource(R.drawable.images__1_);
-             }
-             else if (score == 20)
-             {
-                 cat1.setImageResource(R.drawable.png_transparent_tabby_cat_kitten_open_cat_transparent_background_png_clipart);
+            if (score == 10) {
+                cat1.setImageResource(R.drawable.images__1_);
+            } else if (score == 20) {
+                cat1.setImageResource(R.drawable.catyeas);
 
-             }
+            }
 
 
+        }
+    };
+    View.OnClickListener clickListener2 = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            score--;
+            String s = "Кликов: " + score;
+            TextClick.setText(s.toCharArray(), 0, s.length());
+            ImageView cat1 = findViewById(R.id.cat1);
+            if (score ==9)
+            {
+                cat1.setImageResource(R.drawable.images);
+            }
+            else if(score == 19)
+            {
+                cat1.setImageResource(R.drawable.images__1_);
+            }
 
 
 
         }
 
+
+
     };
+
+
+
+
+
+
+
+
+
 
 
 
